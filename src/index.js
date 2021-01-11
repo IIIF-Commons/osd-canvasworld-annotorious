09ba6016323fdd8c109a7b7c485fe0d1bc210d86
Edward Silverton <e.silverton@gmail.com>
2021-01-11 09:37:01 +0000

    move code to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use the same configuration as Parcel to bundle this sandbox, you can find more
-//   info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// `;
+// https://github.com/UniversalViewer/universalviewer/blob/canvasworld/src/modules/uv-openseadragoncenterpanel-module/OpenSeadragonCenterPanel.ts
+
+(async () => {
+  const json = await manifesto.loadManifest(
+    "http://wellcomelibrary.org/iiif/b18035723/manifest"
+  );
+  
+  const manifest = manifesto.parseManifest(json);
+  const sequence = manifest.getSequenceByIndex(0);
+  const canvases = sequence.getCanvases();
+  
+  let images = await getExternalResources(resources);
+  
+  const layers = [];
+  
+  const viewingDirection = "left-to-right";
+  
+  for (let i = 0; i < images.length; i++) {
+    const data = images[i];
+  
+    let tileSource;
+  
+    if (data.hasServiceDescriptor) {
+      tileSource = data;
+    } else {
+      tileSource = {
+        type: "image",
+        url: data.id,
+        buildPyramid: false
+      };
+    }
+  }
+  
+  var canvasWorld = new manifesto.CanvasWorld(canvases, layers, viewingDirection);
+  
+})();
+
+
+
+function getPagePositions(resources) {
+  const leftPage = resources[0];
+  leftPage.x = 0;
+  const rightPage = resources[1];
+  rightPage.x = leftPage.width;
+  return resources;
+}
